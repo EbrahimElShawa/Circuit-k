00f668ce709be4770b6f5250e29c6a58fbd23e87
Ebrahim ElShawa <imalwaysforlife@gmail.com>
2023-12-28 16:08:11 +0200

    Project GUI

--- Test.py
+++ Test.py
@@ -1,33 +1,12 @@
-import tkinter as tk
+import schemdraw
+import matplotlib.pyplot as plt
+from schemdraw import elements as elm
 
-
-def open_toplevel():
-    # Create a new Toplevel window
-    toplevel = tk.Toplevel(root)
-
-    # Set the title and geometry of the Toplevel window
-    toplevel.title("Toplevel Window")
-    toplevel.geometry("300x200")
-
-    # Create a button in the Toplevel window
-    close_button = tk.Button(toplevel, text="Close Toplevel", command=lambda: close_toplevel(toplevel))
-    close_button.pack(pady=20)
-
-
-def close_toplevel(toplevel):
-    # Close the Toplevel window
-    toplevel.destroy()
-
-    # Focus back on the main Tkinter window
-    root.deiconify()
-
-
-# Create the main Tkinter window
-root = tk.Tk()
-
-# Create a button in the main window to open the Toplevel window
-open_button = tk.Button(root, text="Open Toplevel", command=open_toplevel)
-open_button.pack(pady=20)
-
-# Run the Tkinter event loop
-root.mainloop()
+netlist = [
+    ('R', 'A', 'B', '100K'),
+    ('C', 'B', 'C', '0.1μF'),
+    ('Vs', 'C', 'D', '10V'),
+    ('Is', 'C', 'D', '5A'),
+    ('L', 'C', 'D', '12mH'),
+    ('AC', 'C', 'D', '10V')]
+#  Don't save the circuit, causes stupid problems
--- schematic.py
+++ schematic.py
@@ -3,27 +3,67 @@ from schemdraw import elements
 import schemdraw
 from schemdraw import elements as elm
 
-
-def draw_circuit():
+def draw_circuit_from_netlist(netlist):
     # Create a schemdraw.Drawing object
     d = schemdraw.Drawing()
 
-    with schemdraw.Drawing() as d:
-        d += elm.Resistor().label('100KΩ')
-        d += elm.Capacitor().down().label('0.1μF', loc='bottom')
-        d += elm.Line().left()
-        d += elm.Ground()
-        d += elm.SourceV().up().label('10V')
-        # Save the drawing to a file (optional)
-        d.save('circuit.png')
+    # Dictionary to store component objects and their connecting nodes
+    components = {}
+
+    # Dictionary to store reference points for each node
+    node_positions = {}
+
+    # Parse the netlist and create component objects
+    for component in netlist:
+        component_name, from_node, to_node, value = component
+        if component_name == 'Resistor':
+            comp = elm.Resistor().label(f'{value}Ω')
+        elif component_name == 'Capacitor':
+            comp = elm.Capacitor().label(f'{value}F')
+        elif component_name == 'SourceV':
+            comp = elm.SourceV().label(f'{value}V')
+        else:
+            # Add more component types as needed
+            continue
+
+        # Store the component object
+        components[(from_node, to_node)] = comp
+
+        # Set the reference point for the "from" node
+        if from_node not in node_positions:
+            node_positions[from_node] = d.add(elm.Dot())
+
+    # Organize components into clean loops
+    for (from_node, to_node), comp in components.items():
+        # Draw component connections
+        d.add(node_positions[from_node])
+        d.add(comp)
+        d.add(elm.Line().right().length(0.3))
+        d.add(elm.Dot())
+
+    # Set the title directly on the schemdraw figure
+    d.draw(show=False)
+    plt.title("Circuit Diagram")
+
+    # Set the axis labels to empty strings
+    plt.xticks([])
+    plt.yticks([])
+
+    # Save the drawing to a file (optional
 
     # Display the drawing using matplotlib
-    fig = plt.figure()
-    d.draw(show=False)
-
-    plt.title("Circuit Diagram")
+    plt.grid(False)
+    plt.axis('off')
     plt.show()
 
+# Example netlist with parallel branches
+netlist = [
+    ('Resistor', 'A', 'B', '100K'),
+    ('Capacitor', 'B', 'C', '0.1μ'),
+    ('SourceV', 'C', 'D', '10'),
+    ('Resistor', 'A', 'B', '50K'),
+    ('Capacitor', 'A', 'C', '0.5μ')
+]
 
-if __name__ == "__main__":
-    draw_circuit()
+# Draw the circuit using the netlist
+draw_circuit_from_netlist(netlist)
